71312b36add5e26178c160413ded84cf372b04f4
yuki-nak <dpfcux@gmail.com>
2023-02-15 21:19:59 +0900

    Create template

--- CopyFolders/CopyFolders.ps1
+++ CopyFolders/CopyFolders.ps1
@@ -9,10 +9,16 @@ $list = Import-Csv $PSSCriptRoot\copytarget.csv -Encoding Default
 #$list = Import-Csv $PSSCriptRoot\t_copytarget.csv -Encoding Default
 
 ### Main ###
-
 mkdir $logDir\$logDirName
 
 foreach ($item in $list)
 {
+    $id = $($item.id)
+    $src = $(item.source)
+    $dest = $($item.destination)
+
+    robocopy $rsc $dest /E /PURGE /DCOPY:DAT /R:3 /W:3 /LOG+ $logDir\$logDirname\$id.log
     
+    $logTime = Get-Date -Format "yyyy-MM-dd-HHmm"
+    Add-Content "$logDir\logDirName\Summary.log" "$logTime  `t `: Copy Finished `t $id `t $dest"
 }
