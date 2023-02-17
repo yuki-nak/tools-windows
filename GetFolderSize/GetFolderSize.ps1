6d9554206bd62c11f6f02e8ca167a3822e2e8a77
yuki-nak <dpfcux@gmail.com>
2023-02-18 07:30:49 +0900

    Updated function

--- GetFolderSize/GetFolderSize.ps1
+++ GetFolderSize/GetFolderSize.ps1
@@ -1,11 +1,35 @@
-$ErrorActionPreference = "silentlycontinue"
+﻿$ErrorActionPreference = "silentlycontinue"
 
 Write-Host  "Get folder size and the number of files. It may take some hours if the target is large. Do not close this window before finishing." 
-$targetDir = (Read-Host "Please inpuy the folder path:")
+$targetDir = (Read-Host "Please input the folder path")
 
-$scopeList = Get-ChildItem
+$scopeList = Get-ChildItem $targetDir -Name -Directory
 
-funciton GetFolderSize 
+function GetFolderSize
 {
-    
+    foreach ($scope in $scopeList)
+    {
+        $dirInfo = Get-ChildItem "$targetDir¥$scope" -Recurse | Measure-Object -Property Length -Sum
+        $lastWriteAccess = Get-ItemProperty "$targetDir¥$scope"
+        $item = @{
+            "Path" = $targetDir
+            "Date Scanned" = (Get-Date -Format g)
+            "Folder Name" = $scope
+            "Total GB" = $dirInfo.Sum/(1024*1024*1024)
+            "Total Item" = $dirInfo.Count
+            "Last Access Time" = $lastWriteAccess.LastAccessTime
+        }
+
+        New-Object -TypeName PSObject -Property $item -OutVariable destItem
+        $groupedResult += $destItem
+    }
+
+    $logHistory = $groupedResult | Export-Csv "$PSScriptRoot¥Result_History.csv" -Append -NoTypeInformation -Encoding Default
+    $logLatest = $groupedResult | Export-Csv "$PSScriptRoot¥Result_Latest.csv" -NoTypeInformation -Encoding Default
 }
+
+Write-Host "Scanning now ..."
+GetFolderSize | Out-GridView -Title "Data Size Scan"
+Write-Host "Finished!"
+
+pause
