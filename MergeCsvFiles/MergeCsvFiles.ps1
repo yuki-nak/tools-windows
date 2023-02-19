599d4b29355efd211bda27be7f4eaf54edd62739
yuki-nak <dpfcux@gmail.com>
2023-02-19 16:48:08 +0900

    Initial commit.

---
+++ MergeCsvFiles/MergeCsvFiles.ps1
@@ -0,0 +1,5 @@
+﻿$date = get-date -Format "yyyy-mm-dd"
+$src = "src¥*.csv"
+$dest = "dest¥$date.csv"
+$importFiles = Get-ChildItem $src | foreach {Import-Csv $_}
+$importFiles | Export-Csv -NoTypeInformation $dest
